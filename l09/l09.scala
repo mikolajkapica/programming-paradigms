77bc1de6c747c764d7f67e262a3214dc6933302b
Mikołaj Kapica <mikolajkapica@mail.com>
2024-01-22 12:58:36 +0100

    9 mod

--- l09/l09.scala
+++ l09/l09.scala
@@ -8,7 +8,7 @@ trait Figure {
 
 class Rectangle(private var _a: Double, private var _b: Double = 0) extends Figure {
     if _a <= 0 || _b < 0 then throw new IllegalArgumentException("a and b must be positive")
-    if (b == 0) b = a 
+    if (b == 0) b = a // Square
 
     def a = _a
 
@@ -27,6 +27,7 @@ class Rectangle(private var _a: Double, private var _b: Double = 0) extends Figu
     override def area = a * b
 }
 
+// (-inf, treshold] (treshold, +inf)
 class Splitter(threshold: Double) {
   if (threshold <= 0) throw new IllegalArgumentException("Threshold must be positive")
 
@@ -34,9 +35,17 @@ class Splitter(threshold: Double) {
   private var bigFigures: List[Figure] = Nil
 
   def apply(f: Figure): Unit = {
-    if (f.area < threshold) smallFigures = f :: smallFigures
-    else bigFigures = f :: bigFigures
+    if (isSmaller(f)) smallFigures =  f :: smallFigures 
+    else bigFigures = f :: bigFigures 
   }
+
+  protected def isSmaller(f: Figure): Boolean = f.area <= threshold
+
+  protected def isBigger(f: Figure): Boolean = f.area > threshold
+
+  protected def putIntoSmaller(f: Figure): Unit = smallFigures = f :: smallFigures
+
+  protected def putIntoBigger(f: Figure): Unit = bigFigures = f :: bigFigures
 
   def printSmallFigures(): Unit = {
     println("Small Figures:")
@@ -47,6 +56,25 @@ class Splitter(threshold: Double) {
     println("Big Figures:")
     bigFigures.foreach(f => println(s"Pole: ${f.area}"))
   }
+}
+
+// (-inf, tresholdA] (tresholdA, tresholdB] (tresholdB, +inf)
+class FineSplitter(tresholdA: Double, tresholdB: Double) extends Splitter(tresholdA) {
+  if (!(tresholdA < tresholdB)) throw new IllegalArgumentException("TresholdB has to be bigger B then TresholdA")
+
+  private var mediumFigures: List[Figure] = Nil
+
+  override def apply(f: Figure): Unit = {
+    if tresholdA < f.area && f.area <= tresholdB then putIntoMedium(f) else super.apply(f)
+  }
+
+  protected def isMedium(f: Figure): Boolean = f.area <= tresholdB
+
+  protected def putIntoMedium(f: Figure): Unit = mediumFigures = f :: mediumFigures
+
+  def printMediumFigures(): Unit = 
+    println("Medium Figures")
+    mediumFigures.foreach(f => println(s"Pole: ${f.area}"))
 }
 
 @main def m =
@@ -66,6 +94,25 @@ class Splitter(threshold: Double) {
 
   s.printSmallFigures()
   s.printBigFigures()
+  
+  println("FineSplitter")
+  var s2 = FineSplitter(1000, 3000) 
+  var rectangles2 = List(
+    Rectangle(10, 20),
+    Rectangle(30),
+    Rectangle(30, 40),
+    Rectangle(40, 50),
+    Rectangle(60),
+    Rectangle(60, 70),
+    Rectangle(70, 80),
+  )
+  // rectangles2(1)._a = 5 // error
+  rectangles2(2).a = 5 // actually a_= method is called
+  rectangles2.foreach(s2.apply)
+
+  s2.printSmallFigures()
+  s2.printMediumFigures()
+  s2.printBigFigures()
 
 
     
